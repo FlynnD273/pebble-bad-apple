d6d448428b6b68792b6cd85e6037138ca79a840d
Flynn Duniho <flynn@harveyosity.org>
2025-05-21 23:15:01 -0700

    Quadtree stores the whole animation!

--- rle.py
+++ rle.py
@@ -1,74 +1,55 @@
-from PIL import Image, ImageChops, ImageFilter
+from skimage import io, util, color
 from os import path
+import numpy as np
 import glob
-import pickle
 
 INPUT_PATH = "./frames/"
 OUTPUT_PATH = "./resources/raw/frames"
 
-NUM_FRAMES = 740
+MAX_FILE_SIZE = 256 * 1000
+NUM_FRAMES = -1
 
 files = glob.glob(path.join(INPUT_PATH, "*.png"))
 files.sort()
 files = files if NUM_FRAMES == -1 else files[:NUM_FRAMES]
 
 frame_count = 0
-runs = []
-prev_img = Image.open(files[0]).convert("L").point(lambda p: 255 if p > 20 else 0, "1")  # type: ignore
+bits = []
+
+split_threshold = 0.01
+
+
+def split(img, x, y, w, h):
+    x = int(x)
+    y = int(y)
+    w = int(w)
+    h = int(h)
+    mean = np.mean(img[y : y + h, x : x + w])
+    if not (w < 4 or h < 4) and abs(mean - 0.5) < 0.5 - split_threshold:
+        bits.append(1)
+        split(img, x, y, w / 2, h / 2)
+        split(img, x + w / 2, y, w / 2, h / 2)
+        split(img, x, y + h / 2, w / 2, h / 2)
+        split(img, x + w / 2, y + h / 2, w / 2, h / 2)
+    else:
+        bits.append(0)
+        if mean < 0.5:
+            bits.append(0)
+        else:
+            bits.append(1)
+
 for file in files:
-    img = Image.open(file).convert("L").point(lambda p: 255 if p > 20 else 0, "1")  # type: ignore
-    frame = ImageChops.logical_xor(prev_img, img)
-    prev_img = img
-    (width, height) = img.size
-    is_counting_black = True
-    run_count = 0
-    for y in range(height):
-        for x in range(width):
-            val = frame.getpixel((x, y))
-            is_black = val < 20  # type: ignore
-            if is_counting_black == is_black:
-                run_count += 1
-            else:
-                is_counting_black = not is_counting_black
-                runs.append(run_count)
-                run_count = 1
-    if run_count > 0:
-        runs.append(run_count)
+    img = util.img_as_float(color.rgb2gray(io.imread(file)))
+    (height, width) = img.shape
+    old_bits = bits.copy()
+    split(img, 0, 0, width, height)
+    if len(bits) / 8 > MAX_FILE_SIZE:
+        bits = old_bits
+        break
     frame_count += 1
     print(f"\rFrame {frame_count}", end="     ")
 print()
 
-with open("runs.pickle", "wb") as file:
-    pickle.dump(runs, file)
-
-# with open("runs.pickle", "rb") as file:
-#     runs = pickle.load(file)
-
-bits = []
-
-
-def append(val: int, bit_length: int):
-    for i in range(bit_length - 1, -1, -1):
-        bits.append((val >> i) & 1)
-
-    if val >> bit_length > 0:
-        print("INVALID")
-        exit(1)
-
-
-for run in runs:
-    if run < 0b11:
-        append(0, 2)
-        append(run, 2)
-    elif run < 0xF:
-        append(1, 2)
-        append(run, 4)
-    elif run < 0xFF:
-        append(2, 2)
-        append(run, 8)
-    else:
-        append(3, 2)
-        append(run, 16)
 
 with open(OUTPUT_PATH, "wb") as file:
     acc = 0
